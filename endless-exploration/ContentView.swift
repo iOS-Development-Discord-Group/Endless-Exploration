ece559dedc2b5a8b51436a59e71ccecb56d19752
LK-Simon <lakraven@gmail.com>
2022-08-09 09:41:06 +0200

    Initial Stubs
    Fresh Multi-Platform Project files with no changes yet.

---
+++ endless-exploration/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  endless-exploration
+//
+//  Created by Simon Stuart on 09/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ endless-exploration/endless_explorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  endless_explorationApp.swift
+//  endless-exploration
+//
+//  Created by Simon Stuart on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct endless_explorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
